8ffe55f7a3ff4822fb61fa2b1223fda2b6557f7f
got <bj7908@naver.com>
2019-06-11 09:37:12 +0900

    php200제 7,8문제

---
+++ php200제/7-constant.php
@@ -0,0 +1,9 @@
+<?php
+    //상수 FAVORITE_DOLL에 값 gelatoni를 대입
+    define("FAVORITE_DOLL","gelatoni");
+    echo "상수 FAVORITE_DOLL의 값은 ".FAVORITE_DOLL."<br>";
+
+    //상수 FAVORITE_DOLL에 값 duffy를 대입
+    define("FAVORITE_DOLL","duffy");
+    echo "상수 FAVORITE_DOLL의 값은 ".FAVORITE_DOLL;
+?>
---
+++ php200제/8-operator.php
@@ -0,0 +1,30 @@
+<?php
+    //더하기
+    $sum = 5+5; //5+5를 연산한 값이 변수 sum에 대입
+    echo "<더하기><br>";
+    echo "5더하기 5 는 ".$sum;
+    echo "<br><br>";
+
+    //빼기
+    $minus = 5-3; // 5-3을 연산한 값이 변수 minus에 대입
+    echo "<빼기><br>";
+    echo "5빼기3은 ".$minus;
+    echo "<br><br>";
+
+    //곱하기
+    $mul = 5*3; // 5*3을 연산한 값이 변수 mul에 대입
+    echo "<곱하기><br>";
+    echo "5곱하기3은 ".$mul;
+    echo "<br><br>";
+
+    //나누기
+    $division = 6/3; // 6/3을 연산한 값이 변수 division에 대입
+    echo "<나누기><br>";
+    echo "6나누기3은 ".$division;
+    echo "<br><br>";
+
+    //나머지
+    $rest = 5%3; // 5%3을 연산한 값이 변수 rest에 대입
+    echo "<나머지><br>";
+    echo "5나누기3의 나머지 값은 ".$rest;
+?>
