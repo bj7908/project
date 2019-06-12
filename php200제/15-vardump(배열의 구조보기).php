92c14c37c0443b2c59e508ab21eb0b350d02caa2
tobj <bj7908@naver.com>
2019-06-13 00:40:35 +0900

    php200제 11~15 배열 문제

---
+++ php200제/15-vardump(배열의 구조보기).php
@@ -0,0 +1,20 @@
+<?php
+    $dr = array();
+    $dr['continent'] =array();
+    
+    $dr['continent']['america'] = array();
+    $dr['continent']['america'][0] = '애너하임';
+    $dr['continent']['america'][1] = '올랜도';
+
+    $dr['continent']['asia'] = array();
+    $dr['continent']['asia'][0] = '우라야스';
+    $dr['continent']['asia'][1] = '홍콩';
+    $dr['continent']['asia'][2] = '상하이';
+
+    $dr['continent']['europe'] = array();
+    $dr['continent']['europe'][0] = '파리';
+
+    echo "<pre>";
+    var_dump($dr);
+    echo "</pre>";
+?>
